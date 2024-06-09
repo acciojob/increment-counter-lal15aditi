ff082a795c1c99df92982384c0339f58893427a0
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-06-09 23:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+
+	const counterElement = document.getElementById('counter');
+    
+    const incrementButton = document.getElementById('incrementBtn');
+    
+    incrementButton.addEventListener('click', function() {
+        let currentValue = parseInt(counterElement.textContent);
+        
+        alert(currentValue);
+        
+        currentValue++;
+        
+        counterElement.textContent = currentValue;
+    });
+});
